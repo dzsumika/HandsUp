7da64bae9349aa453a2db8ca83f2651d308d878e
IlDoMyBest <harry.laza@outlook.com>
2017-09-26 15:54:14 +0200

    Changed the alg for printing out the list

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ class classroom:
 	def __init__(self, className): #Defines the name of the classroom
 		self.className = className
 
-	def printList(self): #Prints out the list
-		elev = [""]*10
+	def printList(self, namn): #Changed the "alg to print out list"
 		print "\n"*12
 		print "\t\t\t"+self.className.center(27, "#")
-		print "\t\t\t"+"#" + elev[0].center(25) + "#"
-		print "\t\t\t"+"#" + elev[1].center(25) + "#"
-		print "\t\t\t"+"#" + elev[2].center(25) + "#"
-		print "\t\t\t"+"#" + elev[3].center(25) + "#"
-		print "\t\t\t"+"#" + elev[4].center(25) + "#"
-		print "\t\t\t"+"#" + elev[5].center(25) + "#"
-		print "\t\t\t"+"#" + elev[6].center(25) + "#"
-		print "\t\t\t"+"#" + elev[7].center(25) + "#"
+		print "\t\t\t#\t\t\t  #"
+		for i in range(len(namn)):
+			print "\t\t\t"+"#" + namn[i].center(25) + "#"
 		print "\t\t\t"+"#".center(27, "#")
 		print "\n"*5
+
+tek_cs = classroom("16_tek_cs") #test
+arrayList = ["Harry", "Zimon", "Panos", "Nur", "Ahmed"] #listTest... can be changed...
+tek_cs.protList(arrayList) #test
+
+
+
 
 
